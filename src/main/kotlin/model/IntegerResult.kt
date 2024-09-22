5a7d5cee8aadf9618f81c9d430132dab28ed6f76
random-randoms <daniil.savinov.05@gmail.com>
2024-09-22 20:26:26 +0300

    feat: add result types

---
+++ src/main/kotlin/model/IntegerResult.kt
@@ -0,0 +1,6 @@
+package org.example.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class IntegerResult(val value: Long) : Result
---
+++ src/main/kotlin/model/Result.kt
@@ -0,0 +1,6 @@
+package org.example.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface Result
