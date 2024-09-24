d057a9201398bf7aecead7d4cb96c4d72aa51f58
random-randoms <daniil.savinov.05@gmail.com>
2024-09-24 10:38:25 +0300

    feat: add Result.toString

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,5 @@ package org.example
 import org.example.model.ExpressionParser
 
 fun main() {
-    val res = ExpressionParser("2+3*(4)").parseString()
-    println(res)
+    println(ExpressionParser("2+3*(0)+(((7)))-1").parseString())
 }
--- src/main/kotlin/model/RealResult.kt
+++ src/main/kotlin/model/RealResult.kt
@@ -3,4 +3,6 @@ package org.example.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-class RealResult(val value: Double) : Result
+class RealResult(val value: Double) : Result {
+    override fun toString(): String = value.toString()
+}
