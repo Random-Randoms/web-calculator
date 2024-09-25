3513f6df02c867de89a89a1bb9f6e2c3ac87dfa5
Vasily Fedorov <vasily.fedorov@arsysop.ru>
2024-09-25 09:56:16 +0300

    add expression number

--- src/main/resources/static/scripts/modernStyleCalc.js
+++ src/main/resources/static/scripts/modernStyleCalc.js
@@ -33,15 +33,17 @@ evalButton.onclick = () => {
         body: JSON.stringify(data)
     }).then(async json => {
             let result = await json.text()
-            inputField.value = result
+            inputField.value = result.substring(result.indexOf("#") + 1)
             equalIcon.hidden = false
             progress.hidden = true
+            let id = result.split("#")[0]
 
             var row = table.insertRow(1)
             row.innerHTML =`
+                <td class="max left-align">`+id+`</td>
                 <td class="max left-align">`+user+`</td>
                 <td class="max center-align"><a class="clickableHistory">`+data.expression+`</a></td>
-                <td class="min right-align"><a class="clickableHistory">`+result+`</a></td>`
+                <td class="min right-align"><a class="clickableHistory">`+inputField.value+`</a></td>`
         }
     )
 }
